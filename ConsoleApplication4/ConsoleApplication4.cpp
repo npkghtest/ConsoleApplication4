52123850d6a01278acd9b61b44ad3129641af390
bugor2k <a-perepelica@users.noreply.github.com>
2020-10-20 16:05:56 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Triangle {
+    double    pointA[2],
+        pointB[2],
+        pointC[2];
+};
+
+int main() {
+
+    Triangle t;
+
+    double  a1 = t.pointA[0] = 15,
+        a2 = t.pointA[1] = 10,
+        b1 = t.pointB[0] = 5,
+        b2 = t.pointB[1] = 6;
+
+    cout << sqrt(pow(a1 - b1, 2) + pow(a2 - b2, 2));
+
+    return 0;
+}
